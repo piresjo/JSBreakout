748f1b27ef52105aa1c1afe3369725a6489a2e86
Joe Pires <piresjo@seas.upenn.edu>
2017-02-25 15:37:11 -0500

    Updated Main Files

--- mainGame.js
+++ mainGame.js
@@ -5,41 +5,106 @@ class Brick {
 		this.isHit = false;
 	}
 
+	showXVal() {
+		return this.xVal;
+	}
+
+	showYVal() {
+		return this.yVal;
+	}
+
+	showIsHit() {
+		return this.isHit;
+	}
+
 }
 
 class BlueBrick extends Brick {
-	this.score = 100;
+	constructor(xVal, yVal) {
+		this.score = 100;
+	}
 
+	showScore() {
+		return this.score;
+	}
 }
 
 class RedBrick extends Brick {
-	this.score = 200;
+	constructor(xVal, yVal) {
+		this.score = 200;
+	}
 
+	showScore() {
+		return this.score;
+	}
 }
 
 class GreenBrick extends Brick {
-	this.score = 300;
+	constructor(xVal, yVal) {
+		this.score = 300;
+	}
+
+	showScore() {
+		return this.score;
+	}
 }
 
 class YellowBrick extends Brick {
-	this.score = 400;
+	constructor(xVal, yVal) {
+		this.score = 400;
+	}
+
+	showScore() {
+		return this.score;
+	}
 }
 
 class PurpleBrick extends Brick {
-	this.score = 500;
+	constructor(xVal, yVal) {
+		this.score = 500;
+	}
+
+	showScore() {
+		return this.score;
+	}
 }
 
 class Ball {
 	constructor(xVal, yVal) {
 		this.xVal = xVal;
 		this.yVal = yVal;
-		this.xVelocity;
-		this.yVelocity;
+		this.xVelocity = 4;
+		this.yVelocity = 4;
 		this.isDead = false;
+		this.angle = 180;
 	}
 
 	move() {
 
+	}
+
+	showXVal() {
+		return this.xVal;
+	}
+
+	showYVal() {
+		return this.yVal;
+	}
+
+	showXVelocity() {
+		return this.xVelocity;
+	}
+
+	showYVelocity() {
+		return this.yVelocity;
+	}
+
+	showIsDead() {
+		return this.isDead;
+	}
+
+	showAngle() {
+		return this.angle;
 	}
 }
 
@@ -53,6 +118,14 @@ class Paddle {
 
 	}
 
+	showXVal() {
+		return this.xVal;
+	}
+
+	showYVal() {
+		return this.yVal;
+	}
+
 }
 
 class Game {
@@ -64,4 +137,39 @@ class Game {
 		this.extendLife = 10000;
 	}
 
+	showNumLines() {
+		return this.numLives;
+	}
+
+	showXDem() {
+		return this.xDem;
+	}
+
+	showYDem() {
+		return this.yDem;
+	}
+
+	showScore() {
+		return this.score;
+	}
+
+	showExtendLife() {
+		return this.extendLife;
+	}
+
 }
+
+/*module.exports = {
+  brickClasses: {
+    
+  },
+  ballClasses: {
+    
+  },
+  paddleClasses: {
+    
+  },
+  gameClasses: {
+
+  }
+};*/

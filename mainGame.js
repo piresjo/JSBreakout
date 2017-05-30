57d66815efd8bb148567064c1c9c2717555c22f8
Joe Pires <piresjo@seas.upenn.edu>
2017-05-29 20:00:42 -0400

    Small Updates

--- mainGame.js
+++ mainGame.js
@@ -163,6 +163,8 @@ class Game {
 		this.yDem = yDem;
 		this.score = 0;
 		this.extendLife = 10000;
+		this.fps = 30;
+		this.canvas = null;
 	}
 
 	showNumLines() {
@@ -183,6 +185,63 @@ class Game {
 
 	showExtendLife() {
 		return this.extendLife;
+	}
+
+	init(div) {
+		var self = this;
+		this.containerDiv = div;
+		self.width = window.innerWidth;
+		self.height = window.innerHeight;
+		window.addEventListener('resize', function resize(event) {
+			self.width = window.innerWidth;
+			self.height = window.innerHeight;
+			self.canvas.width = self.width;
+			self.canvas.height = self.height;
+			self.draw();
+		});
+
+		//	Create the canvas.
+		var canvas = document.createElement('canvas');
+		div.appendChild(canvas);
+		this.canvas = canvas;
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+	}
+
+	begin() {
+		//	Create the bricks, set the ball and paddle
+
+		var self = this;
+		//	Start the timer.
+		this.interval = setInterval(function() {
+			self.update();
+			self.draw();	
+		}, 1000 / this.fps);
+	}
+
+	end() {
+		clearInterval(this.interval);
+	}
+
+	update() {
+		var dt = 1 / this.fps;
+		// Update the states of the blocks, the ball, and the object
+	}
+
+	draw() {
+		//	Get the drawing context.
+		var context = this.canvas.getContext("2d");
+
+		//	Draw the background.
+	 	context.fillStyle = '#e3e3e3';
+		context.fillRect(0, 0, this.width, this.height);
+
+		//	Draw  the elements.
+		context.fillStyle = '#ffffff';
+		for(var i = 0; i < this.stars.length; i++) {
+			var star = this.stars[i];
+			context.fillRect(star.x, star.y, star.size, star.size);
+		}
 	}
 
 
